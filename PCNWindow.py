80f73182cd4dd24a24e32413e798fa267013acea
Lily <zx.kaze@gmail.com>
2022-07-24 21:41:54 -0400

    ui updates

--- PCNWindow.py
+++ PCNWindow.py
@@ -2,6 +2,7 @@ from PySide6 import QtWidgets, QtCore, QtGui
 from PCNTab import *
 from SignatureTab import *
 from NotificationTab import *
+from CommentTab import *
 from datetime import datetime
 from string import Template
 import os, sys
@@ -57,19 +58,46 @@ class PCNWindow(QtWidgets.QWidget):
     def initUI(self):
         mainLayout = QtWidgets.QVBoxLayout()
         self.toolbar = QtWidgets.QToolBar()
+        icon_loc = icon = os.path.join(program_location,"icons","save.png")
         self.button_save = QtWidgets.QPushButton("Save")
+        self.button_save.setIcon(QtGui.QIcon(icon_loc))
         self.button_save.clicked.connect(self.save)
+        
+        icon_loc = icon = os.path.join(program_location,"icons","release.png")
         self.button_release = QtWidgets.QPushButton("Release")
+        self.button_release.setIcon(QtGui.QIcon(icon_loc))
+        
+        icon_loc = icon = os.path.join(program_location,"icons","add_comment.png")
+        self.button_comment = QtWidgets.QPushButton("Add Comment")
+        self.button_comment.setIcon(QtGui.QIcon(icon_loc))
+        
+        icon_loc = icon = os.path.join(program_location,"icons","approve.png")
+        self.button_approve = QtWidgets.QPushButton("Approve")
+        self.button_approve.setIcon(QtGui.QIcon(icon_loc))
+        
+        icon_loc = icon = os.path.join(program_location,"icons","reject.png")
+        self.button_reject = QtWidgets.QPushButton("Reject")
+        self.button_reject.setIcon(QtGui.QIcon(icon_loc))
+        
+        icon_loc = icon = os.path.join(program_location,"icons","export.png")
         self.button_export = QtWidgets.QPushButton("Export")
+        self.button_export.setIcon(QtGui.QIcon(icon_loc))
+
         self.toolbar.addWidget(self.button_save)
         self.toolbar.addWidget(self.button_release)
+        self.toolbar.addWidget(self.button_comment)
+        self.toolbar.addWidget(self.button_approve)
+        self.toolbar.addWidget(self.button_reject)
         self.toolbar.addWidget(self.button_export)
+        
         self.tab_widget = QtWidgets.QTabWidget(self)
         self.tab_pcn = PCNTab(self)
+        self.tab_comment = CommentTab(self)
         self.tab_signature = SignatureTab(self,"PCN")
         self.tab_notification = NotificationTab(self)
         
         self.tab_widget.addTab(self.tab_pcn,"PCN")
+        self.tab_widget.addTab(self.tab_comment, "Comments")
         self.tab_widget.addTab(self.tab_signature,"Signatures")
         self.tab_widget.addTab(self.tab_notification,"Notifications")
         mainLayout.addWidget(self.toolbar)
@@ -86,6 +114,10 @@ class PCNWindow(QtWidgets.QWidget):
         
     def save(self):
         try:
+            save_type = "update"
+            if self.doc_id is None:
+                self.generatePCNID()
+                save_type = "insert"
             doc_id = self.tab_pcn.line_id.text()
             doc_type = "PCN"
             author = self.tab_pcn.line_author.text()
@@ -101,15 +133,20 @@ class PCNWindow(QtWidgets.QWidget):
             response = self.tab_pcn.text_response.toHtml()
             modifieddate = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
             #dept = self.tab_ecn.combo_dept.currentText()
-
-            data = (doc_id,doc_type,author,status,title,overview,reason,change,products,replacement,reference,response,modifieddate)
+            
             #overview - text 1, products - text 2, replacement - text 3, reference - text 4, response - text 5
-            self.cursor.execute("INSERT INTO DOCUMENT(DOC_ID,DOC_TYPE,AUTHOR,STATUS,DOC_TITLE,DOC_TEXT_1,DOC_REASON,DOC_SUMMARY,DOC_TEXT_2,DOC_TEXT_3,DOC_TEXT_4,DOC_TEXT_5,LAST_MODIFIED) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?)",(data))
+            if save_type == "insert":
+                data = (doc_id,doc_type,author,status,title,overview,reason,change,products,replacement,reference,response,modifieddate)
+                self.cursor.execute("INSERT INTO DOCUMENT(DOC_ID,DOC_TYPE,AUTHOR,STATUS,DOC_TITLE,DOC_TEXT_1,DOC_REASON,DOC_SUMMARY,DOC_TEXT_2,DOC_TEXT_3,DOC_TEXT_4,DOC_TEXT_5,LAST_MODIFIED) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?)",(data))
+                self.dispMsg("PCN Saved!")
+            else:
+                data = (title,overview,reason,change,products,replacement,reference,response,modifieddate,doc_id)
+                self.cursor.execute("UPDATE DOCUMENT SET DOC_TITLE = ?, DOC_TEXT_1 = ?, DOC_REASON = ?, DOC_SUMMARY = ?, DOC_TEXT_2 = ?, DOC_TEXT_3 = ?, DOC_TEXT_4 = ?, DOC_TEXT_5 = ?, LAST_MODIFIED = ? WHERE DOC_ID = ?",(data))
+                self.dispMsg("PCN Updated!")
             self.db.commit()
-            self.dispMsg("PCN Saved!")
         except Exception as e:
             print(e)
-            self.dispMsg(f"Error occured during data insertion (insertData)!\n Error: {e}")
+            self.dispMsg(f"Error occured during data saving!\n Error: {e}")
             
     def loadData(self):
         self.cursor.execute(f"Select * from DOCUMENT WHERE DOC_ID='{self.doc_id}'")
@@ -127,7 +164,9 @@ class PCNWindow(QtWidgets.QWidget):
         self.tab_pcn.text_response.setHtml(result["DOC_TEXT_5"])
     
     def generatePCNID(self):
-        pass
+        date_time = datetime.now().strftime('%Y%m-%d')
+        self.doc_id = 'PCN-'+date_time[2:]
+        self.tab_pcn.line_id.setText(self.doc_id)
 
     def dispMsg(self,msg):
         msgbox = QtWidgets.QMessageBox()
